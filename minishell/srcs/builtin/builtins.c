31f3f41593b08a39b02e154b71a8a56e00d520b7
qbnt <quentin.banet@hotmail.fr>
2023-11-20 14:30:46 +0100

    oui

--- minishell/srcs/builtin/builtins.c
+++ minishell/srcs/builtin/builtins.c
@@ -14,15 +14,15 @@
 
 int	ft_echo(t_pars *cmds)
 {
-	int	i; //index pour la verif du -n
-	int	flag; //flag pour veif la presence d'un -n
+	int	i;
+	int	flag;
 
-	i = 1; // 1 pour pas check le tiret
+	i = 1;
 	flag = 0;
-	cmds = cmds->next; // le premier c'est echo donc on skip
-	if (cmds->token == 11) // verif de la presence du -n
+	cmds = cmds->next;
+	if (cmds->token == 11)
 	{
-		while (cmds->str[i] == 'n') //si c'est pas un n il rentre pas dedans
+		while (cmds->str[i] == 'n')
 			i++;
 		if (cmds->str[i] != ' ') //si c'est pas un n en premier ni au milieu ca return 
 			return (FAIL);
@@ -37,6 +37,8 @@ int	ft_echo(t_pars *cmds)
 			ft_printf(" ");
 			cmds = cmds->next;
 		}
+		else
+			cmds = cmds->next;
 	}
 	if (flag == 0) //si y'a pas de flag je rajoute une newline
 		ft_printf("\n");
