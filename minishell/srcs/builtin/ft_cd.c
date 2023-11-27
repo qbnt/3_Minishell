f3018c077ecd1e241a9f66db2392407e3117a585
Quentin Puig <qpuig@c2r7p4.42perpignan.fr>
2023-11-27 13:37:06 +0100

    cd_v1

--- minishell/srcs/builtin/ft_cd.c
+++ minishell/srcs/builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: qpuig <qpuig@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 15:38:58 by qpuig             #+#    #+#             */
-/*   Updated: 2023/11/24 14:58:47 by qpuig            ###   ########.fr       */
+/*   Updated: 2023/11/27 13:34:08 by qpuig            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,30 @@ void	ft_cd(t_pars *cmds, t_env *env) // pas sur qu'un pointeur simple mofifie le
 
 	if (cmds->next)
 		cmds = cmds->next;
-	if (chdir(env->env_elems->value) == 0)
+	if (chdir(cmds->str) == 0)
 	{
 		while ((ft_strcmp(env->env_elems->key, "PWD") == FALSE) && env->env_elems->next)
 			env->env_elems = env->env_elems->next;
 		if (ft_strcmp(env->env_elems->key, "PWD") == TRUE)
 		{
 			old = ft_calloc((env->env_elems->value_len + 1), sizeof(char));
-			old = env->env_elems->value;
-			env->env_elems->value = getcwd(env->env_elems->value, 4096);
+			strcpy(old, env->env_elems->value);
+			free(env->env_elems->value);
+			env->env_elems->value = ft_calloc(4097, sizeof(char));
+			getcwd(env->env_elems->value, 4096);
 			env->env_elems = env->env_elems->first;
 			while ((ft_strcmp(env->env_elems->key, "OLDPWD") == FALSE) && env->env_elems->next)
 				env->env_elems = env->env_elems->next;
 			if (ft_strcmp(env->env_elems->key, "OLDPWD") == TRUE)
-				env->env_elems->value = old;
+			{
+				free(env->env_elems->value);
+				env->env_elems->value = ft_calloc((ft_strlen(old) + 1), sizeof(char));
+				ft_strcpy(env->env_elems->value, old);
+			}
 			free(old);
 		}
 	}
+	else 
+		ft_printf("cd: %s: No such file or directory\n", cmds->str);
+	env->env_elems = env->env_elems->first;
 }
